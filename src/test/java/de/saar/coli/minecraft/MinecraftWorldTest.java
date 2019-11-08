973c7d34a35fc4ad391eff1cf54870267a13aa36
Arne Köhn <arne@chark.eu>
2019-11-08 15:25:25 +0100

    fix tests for junit5

--- src/test/java/de/saar/coli/minecraft/MinecraftWorldTest.java
+++ src/test/java/de/saar/coli/minecraft/MinecraftWorldTest.java
@@ -20,7 +20,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import org.eclipse.collections.impl.factory.Lists;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * A simple tes using pre-generated irtg and world.
