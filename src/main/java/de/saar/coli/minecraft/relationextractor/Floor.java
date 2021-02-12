dfc548510ebed3b0a5f6f1e08b89098191d50b49
LaraWoe <lwoerner@coli.uni-saarland.de>
2021-02-12 15:30:05 +0100

    add from-to relations for floor

--- src/main/java/de/saar/coli/minecraft/relationextractor/Floor.java
+++ src/main/java/de/saar/coli/minecraft/relationextractor/Floor.java
@@ -42,4 +42,103 @@ public class Floor extends BigBlock {
   public MutableSet<Relation> generateUnaryRelations(Orientation orientation) {
     return Sets.mutable.of(new Relation("floor", this));
   }
+
+  @Override
+  public MutableSet<Relation> generateRelationsTo(MinecraftObject other, Orientation orientation) {
+    MutableSet<Relation> result = Sets.mutable.empty();
+    var coord = getRotatedCoords(orientation);
+    if ((other instanceof Block)) {
+      var oc =  ((Block) other).getRotatedCoords(orientation);
+
+      // from is if block is at minimal positions
+
+      /*
+       * diagonal 1 runs from front left to back right corner
+       * diagonal 2 runs from front right corner to back left corner
+       */
+
+      if (oc.x1 == coord.getMaxX() && oc.y1 == coord.getMinY() && oc.z1 == coord.getMinZ()) {
+        result.add(new Relation("from-diagonal1",
+            this, other));
+      }
+      if (oc.x1 == coord.getMinX() && oc.y1 == coord.getMinY() && oc.z1 == coord.getMinZ()
+          && coord.getMaxZ() != coord.getMinZ()) {
+        result.add(new Relation("from-diagonal2",
+            this, other));
+      }
+      if (oc.x1 == coord.getMinX() && oc.y1 == coord.getMaxY() && oc.z1 == coord.getMaxZ()) {
+        result.add(new Relation("to-diagonal1", this, other));
+      }
+      if (oc.x1 == coord.getMaxX() && oc.y1 == coord.getMaxY() && oc.z1 == coord.getMaxZ()
+          && coord.getMinZ() != coord.getMaxZ()) {
+        result.add(new Relation("to-diagonal2", this, other));
+      }
+
+      // add also left-of-relations for BigBlock-Block relations
+      var xdistance = coord.getMinX() - oc.x1;
+      var ydistance = coord.getMinY() - oc.y1;
+      var zdistance = oc.z1 - coord.getMaxZ();
+
+      if (xdistance > 0
+          && ydistance == 0
+          && zdistance == 0
+      ) {
+        result.add(new Relation("left-of"+xdistance+"-BigBlock-Block", this, other));
+      }
+      if (xdistance == 0
+          && ydistance > 0
+          && zdistance == 0
+      ) {
+        result.add(new Relation("top-of"+ydistance+"-BigBlock-Block", this, other));
+      }
+      if (xdistance == 0
+          && ydistance == 0
+          && zdistance > 0
+      ) {
+        result.add(new Relation("in-front-of"+zdistance+"-BigBlock-Block", this, other));
+      }
+
+    } // end other==Block
+    else if (other instanceof BigBlock) {
+      var oblock = (BigBlock) other;
+      var ocoords = oblock.getRotatedCoords(orientation);
+
+      var xdistance = coord.getMinX() - ocoords.getMaxX();
+      var ydistance = coord.getMinY() - ocoords.getMaxY();
+      var zdistance = ocoords.getMinZ() - coord.getMaxZ();
+
+      if (this.sameShapeAs(oblock)) {
+
+        if (coord.getMinX() == ocoords.getMinX()
+            && ydistance > 0
+            && ocoords.getMinZ() == coord.getMinZ()
+        ) {
+          result.add(new Relation("top-of-same-shape" + ydistance, this, other));
+        }
+        if (xdistance > 0
+            && ocoords.getMinY() == coord.getMinY() //don't care about y distance, but about minimal y-coordinate being the same
+            && ocoords.getMinZ() == coord.getMinZ()
+        ) {
+          result.add(new Relation("left-of" + xdistance, this, other));
+        }
+        if (coord.getMinX() == ocoords.getMinX()
+            && ocoords.getMinY() == coord.getMinY() //don't care about y distance, but about minimal y-coordinate being the same
+            && zdistance > 0
+        ) {
+          result.add(new Relation("in-front-of" + zdistance, this, other));
+        }
+      }
+      // if length and width are the same and only height differs
+      if (ocoords.x1 - ocoords.x2 == coord.x1 - coord.x2
+          && ocoords.z1 - ocoords.z2 == coord.z1 - coord.z2 ) {
+        if (coord.getMinX() == ocoords.getMinX()
+            && ydistance > 0
+            && ocoords.getMinZ() == coord.getMinZ()
+        ) {
+          result.add(new Relation("top-of"+ydistance, this, other));
+        }
+      }
+    }
+    return result;
+  }
 }
